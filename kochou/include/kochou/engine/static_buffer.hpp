d1642d8b6ceeb87a72f3d19983da43e15d57e376
urlagushka <nikolai.turkin.main@gmail.com>
2025-06-20 05:22:20 +0300

    0_0

--- kochou/include/kochou/engine/static_buffer.hpp
+++ kochou/include/kochou/engine/static_buffer.hpp
@@ -4,14 +4,32 @@
 #include <vulkan/vulkan_raii.hpp>
 #include <glm/glm.hpp>
 
+#include <vulkan_utils/device.hpp>
+
 /*
-kochou::buffer_info info = {
+kochou::buffer_info vertex_info = {
   __device,
   vk::MemoryPropertyFlagBits::eDeviceLocal,
-  4096
+  sizeof(kochou::cube::vertex) * 100,
+  sizeof(kochou::cube::vertex)
 };
+kochou::buffer vertex_buffer(vertex_info);
 
-kochou::buffer vertex_buffer(info);
+kochou::buffer_info index_info = {
+  __device,
+  vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent,
+  sizeof(kochou::cube::index) * 100,
+  sizeof(kochou::cube::index)
+};
+kochou::buffer index_buffer(index_info);
+
+kochou::buffer_info uniform_info = {
+  __device,
+  vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent,
+  sizeof(kochou::cube::uniform) * 100,
+  sizeof(kochou::cube::uniform)
+};
+kochou::buffer uniform_buffer(uniform_info);
 */
 
 namespace kochou
@@ -26,13 +44,21 @@ namespace kochou
     vk::raii::Buffer buffer;
   };
 
+  struct buffer_info
+  {
+    vk::utils::device & dev;
+    vk::MemoryPropertyFlags flags;
+    std::size_t capacity;
+    std::size_t align;
+  };
+
   template< typename T >
   class static_buffer
   {
     using bufferpool = std::vector< memory_block >;
     public:
       static_buffer() = delete;
-      static_buffer(std::size_t cap, vk::MemoryPropertyFlags memflags);
+      static_buffer(const buffer_info & info);
       static_buffer(const static_buffer< T > & rhs);
       static_buffer(static_buffer< T > && rhs);
       static_buffer< T > & operator=(const static_buffer< T > & rhs);
