7520c61d716ff753de47a5992f4d1bcd2650c394
urlagushka <nikolai.turkin.main@gmail.com>
2026-03-19 02:25:46 +0300

    XMAKE

--- kochou/xmake.lua
+++ kochou/xmake.lua
@@ -1,4 +1,63 @@
-set_xmakever("2.8.0")
+local KOCHOU_VULKAN_DYLIB_PATH = {
+    debug   = "/usr/local/lib/libMoltenVK.dylib",
+    release = "libMoltenVK.dylib"
+}
+
+local KOCHOU_WINDOW_BACKEND = {
+    METAL   = "KOCHOU_WINDOW_BACKEND_METAL",
+    WAYLAND = "KOCHOU_WINDOW_BACKEND_WAYLAND",
+    WIN32   = "KOCHOU_WINDOW_BACKEND_WIN32",
+    XCB     = "KOCHOU_WINDOW_BACKEND_XCB",
+    XLIB    = "KOCHOU_WINDOW_BACKEND_XLIB"
+}
+
+local KOCHOU_WINDOW_BACKEND_DEFAULT = {
+    macosx  = KOCHOU_WINDOW_BACKEND.METAL,
+    windows = KOCHOU_WINDOW_BACKEND.WIN32,
+    linux   = KOCHOU_WINDOW_BACKEND.XLIB
+}
+
+function make_defines(target)
+    local window_backend = get_config("window_backend")
+    if not window_backend then
+        window_backend = KOCHOU_WINDOW_BACKEND_DEFAULT[os.host()]
+    end
+
+    local is_backend_valid = false;
+    for _, backend in pairs(KOCHOU_WINDOW_BACKEND) do
+        if backend == window_backend then
+            is_backend_valid = true
+            break
+        end
+    end
+    if not is_backend_valid then
+        -- raise(
+        --     "Invalid window backend: '%s'. Available: %s",
+        --     window_backend,
+        --     table.concat(table.values(KOCHOU_WINDOW_BACKEND), ", ")
+        -- )
+    end
+    target:add("defines", window_backend, {public = true})
+
+    local vulkan_dylib = is_mode("debug") and KOCHOU_VULKAN_DYLIB_PATH.debug or KOCHOU_VULKAN_DYLIB_PATH.release
+    if not vulkan_dylib then
+        raise("Specify vulkan dynamic library path, current: '%s'", vulkan_dylib)
+    end
+    target:add("defines", 'KOCHOU_LOADER_VULKAN_DYNAMIC_LIB_NAME="' .. vulkan_dylib .. '"', {public = true})
+
+    local platform = os.host()
+    if platform == "macosx" then
+        target:add("defines", "KOCHOU_PLATFORM_MACOS", {public = true})
+    elseif platform == "linux" then
+        target:add("defines", "KOCHOU_PLATFORM_LINUX", {public = true})
+    elseif platform == "windows" then
+        target:add("defines", "KOCHOU_PLATFORM_WIN32", {public = true})
+    else
+        raise("Unsupported platform: '%s'", platform)
+    end
+end
+
+set_xmakever("3.0.0")
 set_project("kochou")
 set_languages("c++23")
 
@@ -21,3 +80,5 @@ target("kochou")
     if is_plat("macosx") then
         add_frameworks("Cocoa", "Metal", "QuartzCore", "Foundation")
     end
+
+    on_load(make_defines)
--- xmake.lua
+++ xmake.lua
@@ -1,73 +1,4 @@
--- defines
-
 local PROJECT_NAME = "example"
-
-local KOCHOU_VULKAN_DYLIB_PATH = {
-    debug   = "/usr/local/lib/libMoltenVK.dylib",
-    release = "libMoltenVK.dylib"
-}
-
-local KOCHOU_WINDOW_BACKEND = {
-    METAL   = "KOCHOU_WINDOW_BACKEND_METAL",
-    WAYLAND = "KOCHOU_WINDOW_BACKEND_WAYLAND",
-    WIN32   = "KOCHOU_WINDOW_BACKEND_WIN32",
-    XCB     = "KOCHOU_WINDOW_BACKEND_XCB",
-    XLIB    = "KOCHOU_WINDOW_BACKEND_XLIB"
-}
-
-local KOCHOU_WINDOW_BACKEND_DEFAULT = {
-    macosx  = KOCHOU_WINDOW_BACKEND.METAL,
-    windows = KOCHOU_WINDOW_BACKEND.WIN32,
-    linux   = KOCHOU_WINDOW_BACKEND.XLIB
-}
-
--- functions
-
-function make_window_backend(target)
-    local window_backend = get_config("window_backend")
-    if not window_backend then
-        window_backend = KOCHOU_WINDOW_BACKEND_DEFAULT[os.host()]
-    end
-
-    local is_backend_valid = false;
-    for _, backend in pairs(KOCHOU_WINDOW_BACKEND) do
-        if backend == window_backend then
-            is_backend_valid = true
-            break
-        end
-    end
-    if not is_backend_valid then
-        -- raise(
-        --     "Invalid window backend: '%s'. Available: %s",
-        --     window_backend,
-        --     table.concat(table.values(KOCHOU_WINDOW_BACKEND), ", ")
-        -- )
-    end
-    target:add("defines", window_backend)
-end
-
-function make_vulkan_dylib(target)
-    local vulkan_dylib = is_mode("debug") and KOCHOU_VULKAN_DYLIB_PATH.debug or KOCHOU_VULKAN_DYLIB_PATH.release
-    if not vulkan_dylib then
-        raise("Specify vulkan dynamic library path, current: '%s'", vulkan_dylib)
-    end
-    target:add("defines", "KOCHOU_LOADER_VULKAN_DYNAMIC_LIB_NAME=" .. vulkan_dylib)
-end
-
-function make_platform(target)
-    local platform = os.host()
-    if platform == "macosx" then
-        target:add("defines", "KOCHOU_PLATFORM_MACOS")
-    elseif platform == "linux" then
-        target:add("defines", "KOCHOU_PLATFORM_LINUX")
-    elseif platform == "windows" then
-        target:add("defines", "KOCHOU_PLATFORM_WIN32")
-    else
-        raise("Unsupported platform: '%s'", platform)
-    end
-end
-
--- xmake
 
 set_xmakever("3.0.0")
 set_project(PROJECT_NAME)
@@ -91,9 +22,5 @@ target(PROJECT_NAME)
     add_files("main.cpp")
     add_deps("kochou")
     add_packages("glm")
-
-    on_load(make_window_backend)
-    on_load(make_vulkan_dylib)
-    on_load(make_platform)
 
     set_default(true)
