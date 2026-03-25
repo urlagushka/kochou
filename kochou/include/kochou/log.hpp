f709f56adc6f230951791d999793146b34d1f6b4
urlagushka <nikolai.turkin.main@gmail.com>
2026-03-25 03:00:52 +0300

    log

--- kochou/include/kochou/log.hpp
+++ kochou/include/kochou/log.hpp
@@ -7,70 +7,108 @@
 #include <source_location>
 #include <string_view>
 
-#include <ktl/errc.hpp>
-#include <ktl/flat_set.hpp>
-
-namespace ktl
+namespace kochou::log
 {
-using log_tim_type = std::chrono::time_point< std::chrono::system_clock >;
-using log_ctx_type = std::source_location;
-using log_msg_type = std::string;
-using log_lvl_type = ktl::err_level;
-using log_err_type = ktl::errc;
-
-struct log_data final
+enum class lvl_type : ktl::u8
 {
-    log_tim_type tim;
-    log_ctx_type ctx;
-    log_msg_type msg;
-    log_lvl_type lvl;
-    log_err_type err;
-};
-
-enum class log_level
-{
-    debug   = 0,
     info    = 1,
     warning = 2,
     error   = 3,
-    fatal   = 4
+    fatal   = 4,
+    debug   = 5,
 };
+using tim_type = std::chrono::time_point< std::chrono::system_clock >;
+using ctx_type = std::source_location;
+using msg_type = std::string_view;
 
-[[maybe_unused]] constexpr std::string
-format_log(const log_data &);
+[[maybe_unused]] inline msg_type
+format_log(const tim_type & _tim, const ctx_type & _ctx, const msg_type & _msg, const msg_type & _lvl)
+{
+    msg_type ctx_msg = std::format("");
+    return std::format("[KOCHOU] {} {} {} {}", _lvl, _tim, ctx_msg, _msg);
+}
 
-[[maybe_unused]] constexpr std::string
-format_log(const log_tim_type &, const log_ctx_type &, const log_msg_type &, const log_lvl_type &,
-           const log_err_type &);
+[[maybe_unused]] inline consteval bool
+exit_condition(lvl_type _lvl)
+{
+    return static_cast< ktl::u8 >(_lvl) > KOCHOU_LOG_LEVEL;
+}
 
 #ifdef KOCHOU_BUILD_DEBUG
 [[maybe_unused]] inline void
-log(log_level _level, std::string_view _msg, log_ctx_type _ctx = log_ctx_type::current())
+debug(msg_type _msg, ctx_type _ctx = ctx_type::current())
 {
-    switch (_level)
+    if constexpr (!exit_condition(lvl_type::debug))
     {
-    case log_level::debug:
-        break;
-    case log_level::info:
-        break;
-    case log_level::warning:
-        break;
-    case log_level::error:
-        break;
-    case log_level::fatal:
-        break;
-    default:
-        break;
+        std::cout << format_log(std::chrono::system_clock::now(), _ctx, _msg, "DEBUG") << std::endl;
     }
 }
+
+[[maybe_unused]] inline void
+info(msg_type _msg, ctx_type _ctx = ctx_type::current())
+{
+    if constexpr (!exit_condition(lvl_type::info))
+    {
+        std::cout << format_log(std::chrono::system_clock::now(), _ctx, _msg, "INFO") << std::endl;
+    }
+}
+
+[[maybe_unused]] inline void
+warning(msg_type _msg, ctx_type _ctx = ctx_type::current())
+{
+    if constexpr (!exit_condition(lvl_type::warning))
+    {
+        std::cout << format_log(std::chrono::system_clock::now(), _ctx, _msg, "WARNING") << std::endl;
+    }
+}
+
+[[maybe_unused]] inline void
+error(msg_type _msg, ctx_type _ctx = ctx_type::current())
+{
+    if constexpr (!exit_condition(lvl_type::error))
+    {
+        std::cout << format_log(std::chrono::system_clock::now(), _ctx, _msg, "ERROR") << std::endl;
+    }
+}
+
+[[maybe_unused]] inline void
+fatal(msg_type _msg, ctx_type _ctx = ctx_type::current())
+{
+    if constexpr (!exit_condition(lvl_type::fatal))
+    {
+        std::cout << format_log(std::chrono::system_clock::now(), _ctx, _msg, "FATAL") << std::endl;
+    }
+}
+
 #elifdef KOCHOU_BUILD_RELEASE
 [[maybe_unused]] inline void
-log(log_level, std::string_view, log_ctx_type)
+debug(msg_type, ctx_type)
 {
 }
+
+[[maybe_unused]] inline void
+info(msg_type, ctx_type)
+{
+}
+
+[[maybe_unused]] inline void
+warning(msg_type, ctx_type)
+{
+}
+
+[[maybe_unused]] inline void
+error(msg_type, ctx_type)
+{
+}
+
+[[maybe_unused]] inline void
+fatal(msg_type, ctx_type)
+{
+}
+
 #else
 static_assert(false && "kochou build mode unspecified!");
 #endif
-} // namespace ktl
+} // namespace kochou::log
 
 #endif
